a9b8f88b79c3346af40d54ae9a23aef583951493
David Haiman <dhaiman2@gmail.com>
2018-04-28 23:21:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ function placeOrder(cardNumber) {
     return "Sorry, we don't have a credit card on file for you.";
   }
   
-  cart.length = 0;
+  cart;
   
   return `Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`;
 }
